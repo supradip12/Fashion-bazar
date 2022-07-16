4ee3537804a2347e6867990b15245edc043a1c03
supradip <supradipdey1@gmail.com>
2022-07-16 10:42:46 +0530

    Initial commit

---
+++ admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKX9-L0mXEBEvztcK8ib4XSIk8uHMGfeQ",
+  authDomain: "supra-e340f.firebaseapp.com",
+  projectId: "supra-e340f",
+  storageBucket: "supra-e340f.appspot.com",
+  messagingSenderId: "1061980652253",
+  appId: "1:1061980652253:web:84e30194815375e9201134",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;

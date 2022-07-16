19500ec309ea72b69d9fd778720fd3e1dda03157
Supradip Dey <95077034+supradip12@users.noreply.github.com>
2022-07-16 11:06:12 +0530

    Update Cart.jsx

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -248,7 +248,7 @@ const Cart = () => {
               <SummaryItemPrice>$ {cart.total}</SummaryItemPrice>
             </SummaryItem>
             <StripeCheckout
-              name="Lama Shop"
+              name="Supra Store"
               image="https://avatars.githubusercontent.com/u/1486366?v=4"
               billingAddress
               shippingAddress
